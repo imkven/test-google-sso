cc3e3fd3e74d713341d994543331c6c7f725013a
Kven Ho <k2command@gmail.com>
2022-05-18 16:10:45 +0800

    Update page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { GoogleLogin } from 'react-google-login';
 
 function App() {
+  const [loginData, setLoginData] = useState({});
+  const [profileData, setProfileData] = useState({});
+
+  const handleFailure = (response) => {
+    console.error(response);
+  };
+  
+  const handleLogin = async (googleData) => {
+    console.log('googleData', googleData);
+    const res = await fetch('http://localhost:3001/login-with-google', {
+      method: 'POST',
+      body: JSON.stringify({
+        token: googleData.tokenObj,
+        tokenId: googleData.tokenId,
+        accessToken: googleData.accessToken,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await res.json();
+    if (data.loggedIn) {
+      setProfileData(data.profile);
+      setLoginData({ token: googleData.tokenId });
+      localStorage.setItem('loginData', JSON.stringify(data));
+    } else {
+      setProfileData({});
+      setLoginData({});
+    }
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('loginData');
+    setLoginData({});
+    setProfileData({});
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loginData.token && profileData.name ? (
+        <div>
+          <h3>Hi, {profileData.name}</h3>
+          <p>You logged in as {profileData.email}</p>
+          <div><img alt={profileData.name} src={profileData.picture} /></div>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <GoogleLogin
+          clientId="xxxxxxxxxxxx.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={handleLogin}
+          onFailure={handleFailure}
+          cookiePolicy={'single_host_origin'}
+        />
+      )}      
     </div>
   );
 }
